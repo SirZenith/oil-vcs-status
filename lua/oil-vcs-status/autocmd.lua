12fd509205ccb8c425672f333946781b03a47de8
陆离 <sirzenith@163.com>
2024-02-15 19:36:56 +0800

    refine(autocmd): use timer for autocommand debounce

--- lua/oil-vcs-status/autocmd.lua
+++ lua/oil-vcs-status/autocmd.lua
@@ -6,25 +6,37 @@ local loop = vim.loop
 local M = {}
 
 local AUGROUP_NAME = "oil-vcs-status"
-local BUF_MODIFY_DEBOUNCE = 200
+local BUF_MODIFY_DEBOUNCE = 100
+
+local watched_buf = {}
 
 ---@param bufnr integer
 ---@param augroup any
 local function setup_buf_change_watcher(bufnr, augroup)
-    local last_trigger = 0
+    if watched_buf[bufnr] then
+        return
+    end
+
+    watched_buf[bufnr] = true
+    local timer
 
     api.nvim_create_autocmd("BufModifiedSet", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-            local now = loop.now()
-            local delta_time = now - last_trigger
-            if delta_time < BUF_MODIFY_DEBOUNCE then
-                return
+            if timer then
+                timer:stop()
+            else
+                timer = loop.new_timer()
             end
 
-            last_trigger = now
-            status.update_status(bufnr)
+            local callback = vim.schedule_wrap(function()
+                timer:stop()
+                timer:close()
+                timer = nil
+                status.update_status(bufnr)
+            end)
+            timer:start(BUF_MODIFY_DEBOUNCE, 0, callback)
         end,
     })
 end

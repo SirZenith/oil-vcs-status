31eee7981defc150985665018c98ae2fb0c2e554
陆离 <sirzenith@163.com>
2024-02-15 20:24:00 +0800

    refine(config): change default symbol priority

--- lua/oil-vcs-status/config.lua
+++ lua/oil-vcs-status/config.lua
@@ -11,16 +11,16 @@ local M = {
     -- Sign character used by each status.
     ---@type table<oil-vcs-status.StatusType, string>
     status_symbol = {
-        [StatusType.Added]       = "A",
-        [StatusType.Copied]      = "C",
-        [StatusType.Deleted]     = "D",
-        [StatusType.Ignored]     = "!",
-        [StatusType.Modified]    = "M",
-        [StatusType.Renamed]     = "R",
-        [StatusType.TypeChanged] = "T",
-        [StatusType.Unmodified]  = " ",
-        [StatusType.Unmerged]    = "U",
-        [StatusType.Untracked]   = "?",
+        [StatusType.Added]               = "A",
+        [StatusType.Copied]              = "C",
+        [StatusType.Deleted]             = "D",
+        [StatusType.Ignored]             = "!",
+        [StatusType.Modified]            = "M",
+        [StatusType.Renamed]             = "R",
+        [StatusType.TypeChanged]         = "T",
+        [StatusType.Unmodified]          = " ",
+        [StatusType.Unmerged]            = "U",
+        [StatusType.Untracked]           = "?",
 
         [StatusType.UpstreamAdded]       = "A",
         [StatusType.UpstreamCopied]      = "C",
@@ -37,16 +37,16 @@ local M = {
     -- Highlight group name used by each status type.
     ---@type table<oil-vcs-status.StatusType, string | false>
     status_hl_group = {
-        [StatusType.Added]       = "OilVcsStatusAdded",
-        [StatusType.Copied]      = "OilVcsStatusCopied",
-        [StatusType.Deleted]     = "OilVcsStatusDeleted",
-        [StatusType.Ignored]     = "OilVcsStatusIgnored",
-        [StatusType.Modified]    = "OilVcsStatusModified",
-        [StatusType.Renamed]     = "OilVcsStatusRenamed",
-        [StatusType.TypeChanged] = "OilVcsStatusTypeChanged",
-        [StatusType.Unmodified]  = "OilVcsStatusUnmodified",
-        [StatusType.Unmerged]    = "OilVcsStatusUnmerged",
-        [StatusType.Untracked]   = "OilVcsStatusUntracked",
+        [StatusType.Added]               = "OilVcsStatusAdded",
+        [StatusType.Copied]              = "OilVcsStatusCopied",
+        [StatusType.Deleted]             = "OilVcsStatusDeleted",
+        [StatusType.Ignored]             = "OilVcsStatusIgnored",
+        [StatusType.Modified]            = "OilVcsStatusModified",
+        [StatusType.Renamed]             = "OilVcsStatusRenamed",
+        [StatusType.TypeChanged]         = "OilVcsStatusTypeChanged",
+        [StatusType.Unmodified]          = "OilVcsStatusUnmodified",
+        [StatusType.Unmerged]            = "OilVcsStatusUnmerged",
+        [StatusType.Untracked]           = "OilVcsStatusUntracked",
 
         [StatusType.UpstreamAdded]       = "OilVcsStatusUpstreamAdded",
         [StatusType.UpstreamCopied]      = "OilVcsStatusUpstreamCopied",
@@ -62,11 +62,18 @@ local M = {
 
     -- Sign priority of each staus. When sign column width is less then staus
     -- symbol number, symbol with higher priority will be shown.
+    -- If signcolumn is wide enough, signs will be display from left to right in
+    -- order of priority from low to high.
     ---@type table<oil-vcs-status.StatusType, number>
     status_priority = {
         [StatusType.UpstreamIgnored]     = 0,
-        [StatusType.UpstreamUnmodified]  = 1,
-        [StatusType.UpstreamUntracked]   = 2,
+        [StatusType.Ignored]             = 0,
+
+        [StatusType.UpstreamUntracked]   = 1,
+        [StatusType.Untracked]           = 1,
+
+        [StatusType.UpstreamUnmodified]  = 2,
+        [StatusType.Unmodified]          = 2,
 
         [StatusType.UpstreamCopied]      = 3,
         [StatusType.UpstreamRenamed]     = 3,
@@ -78,21 +85,15 @@ local M = {
 
         [StatusType.UpstreamUnmerged]    = 5,
 
-        -- --------------------------------------------------------------------
+        [StatusType.Copied]              = 13,
+        [StatusType.Renamed]             = 13,
+        [StatusType.TypeChanged]         = 13,
 
-        [StatusType.Ignored]     = 10,
-        [StatusType.Unmodified]  = 11,
-        [StatusType.Untracked]   = 12,
+        [StatusType.Deleted]             = 14,
+        [StatusType.Modified]            = 14,
+        [StatusType.Added]               = 14,
 
-        [StatusType.Copied]      = 13,
-        [StatusType.Renamed]     = 13,
-        [StatusType.TypeChanged] = 13,
-
-        [StatusType.Deleted]     = 14,
-        [StatusType.Modified]    = 14,
-        [StatusType.Added]       = 14,
-
-        [StatusType.Unmerged]    = 15,
+        [StatusType.Unmerged]            = 15,
     },
 }
 
